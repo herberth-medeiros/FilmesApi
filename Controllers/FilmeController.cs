6507139b92bdb44e6ba1af320f406a654f6d47cc
seven-beto <herberthfreitas26@gmail.com>
2025-05-03 17:55:45 -0300

    Feat: Endpoints de cadastro e listagem criados

---
+++ Controllers/FilmeController.cs
@@ -0,0 +1,26 @@
+﻿using FilmesApi.Entity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FilmeController : ControllerBase
+{
+    private static List<Filme> filmes = new List<Filme>();
+
+    [HttpGet]
+    public List<Filme> RecuperaFilmes()
+    {
+        return filmes;
+    }
+
+    [HttpPost]
+    public void AdicionarFilme([FromBody] Filme filme)
+    {
+        filmes.Add(filme);
+        Console.WriteLine(filme.Titulo);
+        Console.WriteLine(filme.Genero);
+        Console.WriteLine(filme.Duracao);
+    }
+}
---
+++ Entity/Filme.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesApi.Entity
+{
+    public class Filme
+    {
+
+        [Required(ErrorMessage = "O titulo do filme e obrigatorio")]
+        public string Titulo { get; set; }
+
+        [Required(ErrorMessage = "O genero e obrigatorio")]
+        [MaxLength(50, ErrorMessage = "O tamanho do genero nao pode exceder 50 caracteres")]
+        public string Genero { get; set; }
+
+        [Required]
+        [Range(70,600, ErrorMessage = "A duracao deve ser entre 70 e 600 minutos")]
+        public int Duracao { get; set; }
+
+  }
+}
